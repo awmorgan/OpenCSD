72511b0bc05f3f9755599b85a1dcacb15fa82aeb
Bill Morgan <arthurwilliammorgan@gmail.com>
2026-03-05 19:02:29 -0600

    etmv4: enable armv8_1m_branches snapshot test

--- opencsd/internal/etmv4/snapshot_test.go
+++ opencsd/internal/etmv4/snapshot_test.go
@@ -65,7 +65,7 @@ func TestETMv4SnapshotsAgainstGolden(t *testing.T) {
 	}{
 		{name: "juno_r1_1", sourceName: "ETB_0", traceIDs: []string{"10", "11", "12", "13", "14", "15"}},
 		{name: "a57_single_step", sourceName: "CSTMC_TRACE_FIFO", traceIDs: []string{"10"}},
-		// {name: "armv8_1m_branches", sourceName: "etr_0", traceIDs: []string{"0"}},
+		{name: "armv8_1m_branches", sourceName: "etr_0", traceIDs: []string{"0"}},
 		// {name: "juno-uname-001", sourceName: "ETB_0", traceIDs: []string{"10"}},
 		// {name: "juno-uname-002", sourceName: "ETB_0", traceIDs: []string{"10", "12", "14", "16", "18", "1a"}},
 		// {name: "juno-ret-stck", sourceName: "ETB_0", traceIDs: []string{"10", "11", "12", "13", "14", "15"}},
